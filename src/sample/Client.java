71841b493c64f66fb9212856f6b6c7461edce142
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-07-11 18:21:27 +0100

    first commit

---
+++ src/sample/Client.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.scene.layout.VBox;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+
+}

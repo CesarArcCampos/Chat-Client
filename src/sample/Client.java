d3cf83bbf20580cabdd3efd0ab0922b051d30eaf
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-07-11 18:24:55 +0100

    Add Client implementation

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -7,5 +7,67 @@ import java.net.Socket;
 
 public class Client {
 
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
+    public Client(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            System.out.println("Failed to create Client");
+            e.printStackTrace();
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessageToServer(String messageToServer) {
+        try{
+            bufferedWriter.write(messageToServer);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to send message to Server");
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void receiveMessageFromServer(VBox vBox) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (socket.isConnected()) {
+                    try {
+                        String messageFromServer = bufferedReader.readLine();
+                        Controller.addLabel(messageFromServer, vBox);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Failed to receive message from Server");
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                        break;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
